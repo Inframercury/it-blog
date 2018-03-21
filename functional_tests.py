3be8a39a238dbe5e7729c2e03e76d6eb146e0f12
Phil <inframercury@gmail.com>
2018-03-21 18:27:32 +0700

    Edit functional tests file

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,16 @@
 from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    
+
 
 browser = webdriver.Firefox()
 browser.get('http://127.0.0.1:8000/')
