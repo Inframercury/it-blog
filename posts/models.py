8362ded6330a7f5060aeb3246c54f2e0b319b437
Phil <inframercury@gmail.com>
2018-03-21 18:22:53 +0700

    Edit Post model

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.title
 
 
 
