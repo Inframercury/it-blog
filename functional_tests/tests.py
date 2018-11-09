fc1158d22b6f4bc1edc4e9e5505608a185f7fab0
Phil <inframercury@gmail.com>
2018-11-10 01:06:59 +0700

    Fix functional tests logic

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,10 +9,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_user_visits_blog(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        assert 'IT Blog' in self.browser.title
+
+
     
 
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000/')
 
-assert 'It Blog' in browser.title
