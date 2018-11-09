a335a9debd1d1111f3cf9279df52e7d12c12d1b3
Phil <inframercury@gmail.com>
2018-11-10 00:39:31 +0700

    Add post creation logit to test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        Post.objects.create(title='First Post', content="Some Content")
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'posts/post_list.html')
